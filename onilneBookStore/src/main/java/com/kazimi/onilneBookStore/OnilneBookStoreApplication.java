cb10708d9c4cf89f5fdc808d8ee444ecb5f21c96
rohullah1 <rohullah.kn@gmail.com>
2020-06-07 15:46:42 +0430

    this is SpringBoot configuration and properties files!
    Change-Id: I163b467fed000bc2a55e2867ff357f64f669125b

---
+++ onilneBookStore/src/main/java/com/kazimi/onilneBookStore/OnilneBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.kazimi.onilneBookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnilneBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnilneBookStoreApplication.class, args);
+	}
+
+}
